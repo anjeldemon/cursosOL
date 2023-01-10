371a994561243aa7baea607609cd36d386a37258
anjeldemon <yairmillan1@gmail.com>
2023-01-10 16:41:02 -0500

    correccion de bd

--- codigobd/cursosonline.sql
+++ codigobd/cursosonline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-12-2022 a las 00:18:23
+-- Tiempo de generación: 10-01-2023 a las 22:40:11
 -- Versión del servidor: 10.4.24-MariaDB
 -- Versión de PHP: 8.1.6
 
@@ -105,6 +105,13 @@ CREATE TABLE `registrosuscripcion` (
   `tiempoSuscripcionid` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Volcado de datos para la tabla `registrosuscripcion`
+--
+
+INSERT INTO `registrosuscripcion` (`reg`, `fechaDeSuscripcion`, `fechaFinSuscripcion`, `usuarioid`, `tiempoSuscripcionid`) VALUES
+(9, '2023-01-10', '2024-01-10', 1111, 365);
+
 -- --------------------------------------------------------
 
 --
@@ -121,19 +128,9 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `rol`) VALUES
+(0, 'default'),
 (1, 'admin'),
 (2, 'user');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `roles_usuario`
---
-
-CREATE TABLE `roles_usuario` (
-  `rolesid` int(10) NOT NULL,
-  `usuarioid` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -151,8 +148,8 @@ CREATE TABLE `tiemposuscripcion` (
 --
 
 INSERT INTO `tiemposuscripcion` (`id`, `cantidadTiempo`) VALUES
-(1, '1 año'),
-(6, '6 meses');
+(180, '6 meses'),
+(365, '1 año');
 
 -- --------------------------------------------------------
 
@@ -165,19 +162,48 @@ CREATE TABLE `usuario` (
   `nombres` varchar(45) NOT NULL,
   `apellidos` varchar(45) NOT NULL,
   `correo` varchar(45) NOT NULL,
-  `telefono` int(11) NOT NULL,
+  `telefono` bigint(11) NOT NULL,
   `fechaDeNacimiento` date NOT NULL,
   `pass` varchar(255) NOT NULL,
-  `generoid` int(10) NOT NULL
+  `generoid` int(10) NOT NULL,
+  `rol_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id`, `nombres`, `apellidos`, `correo`, `telefono`, `fechaDeNacimiento`, `pass`, `generoid`) VALUES
-(1234, 'usuario', 'prueba', 'prueba@prueba.com', 2147483647, '2022-12-29', '81dc9bdb52d04dc20036dbd8313ed055', 3),
-(5678, 'admin', 'prueba', 'prueba@prueba.com', 5678, '2022-12-30', '674f3c2c1a8a6f90461e8a66fb5550ba', 1);
+INSERT INTO `usuario` (`id`, `nombres`, `apellidos`, `correo`, `telefono`, `fechaDeNacimiento`, `pass`, `generoid`, `rol_id`) VALUES
+(1002, 'usu002', 'prueba002', 'prueba@prueba.com', 3000000002, '2000-01-02', 'fba9d88164f3e2d9109ee770223212a0', 3, 0),
+(1003, 'usu003', 'prueba003', 'prueba@prueba.com', 3000000003, '2000-01-03', 'aa68c75c4a77c87f97fb686b2f068676', 3, 0),
+(1004, 'usu004', 'prueba004', 'prueba@prueba.com', 3000000004, '2000-01-04', 'fed33392d3a48aa149a87a38b875ba4a', 3, 0),
+(1005, 'usu005', 'prueba005', 'prueba@prueba.com', 3000000005, '2000-01-05', '2387337ba1e0b0249ba90f55b2ba2521', 3, 0),
+(1006, 'usu006', 'prueba006', 'prueba@prueba.com', 3000000006, '2000-01-06', '1006', 3, 0),
+(1007, 'usu007', 'prueba007', 'prueba@prueba.com', 3000000007, '2000-01-07', '1007', 3, 0),
+(1008, 'usu008', 'prueba008', 'prueba@prueba.com', 3000000008, '2000-01-08', '1008', 3, 0),
+(1009, 'usu009', 'prueba009', 'prueba@prueba.com', 3000000009, '2000-01-09', '1009', 3, 0),
+(1010, 'usu010', 'prueba010', 'prueba@prueba.com', 3000000010, '2000-01-10', '1010', 3, 0),
+(1011, 'usu011', 'prueba011', 'prueba@prueba.com', 3000000011, '2000-01-11', '1011', 3, 0),
+(1012, 'usu012', 'prueba012', 'prueba@prueba.com', 3000000012, '2000-01-12', '1012', 3, 0),
+(1013, 'usu013', 'prueba013', 'prueba@prueba.com', 3000000013, '2000-01-13', '1013', 3, 0),
+(1014, 'usu014', 'prueba014', 'prueba@prueba.com', 3000000014, '2000-01-14', '1014', 3, 0),
+(1015, 'usu015', 'prueba015', 'prueba@prueba.com', 3000000015, '2000-01-15', '1015', 3, 0),
+(1016, 'usu016', 'prueba016', 'prueba@prueba.com', 3000000016, '2000-01-16', '1016', 3, 0),
+(1017, 'usu017', 'prueba017', 'prueba@prueba.com', 3000000017, '2000-01-17', '1017', 3, 0),
+(1018, 'usu018', 'prueba018', 'prueba@prueba.com', 3000000018, '2000-01-18', '1018', 3, 0),
+(1019, 'usu019', 'prueba019', 'prueba@prueba.com', 3000000019, '2000-01-19', '1019', 3, 0),
+(1020, 'usu020', 'prueba020', 'prueba@prueba.com', 3000000020, '2000-01-20', '65cc2c8205a05d7379fa3a6386f710e1', 3, 0),
+(1021, 'usu021', 'prueba021', 'prueba@prueba.com', 3000000021, '2000-01-21', '1021', 3, 0),
+(1022, 'usu022', 'prueba022', 'prueba@prueba.com', 3000000022, '2000-01-22', '1022', 3, 0),
+(1023, 'usu023', 'prueba023', 'prueba@prueba.com', 3000000023, '2000-01-23', '1023', 3, 0),
+(1024, 'usu024', 'prueba024', 'prueba@prueba.com', 3000000024, '2000-01-24', '81dc9bdb52d04dc20036dbd8313ed055', 3, 0),
+(1025, 'usu025', 'prueba025', 'prueba@prueba.com', 3000000025, '2000-01-25', '81dc9bdb52d04dc20036dbd8313ed055', 3, 0),
+(1026, 'usu026', 'prueba026', 'prueba@prueba.com', 3000000026, '2000-01-26', '81dc9bdb52d04dc20036dbd8313ed055', 3, 0),
+(1111, 'usuario2', 'prueba1', 'prueba@prueba.com', 5815759, '2022-12-29', 'd93591bdf7860e1e4ee2fca799911215', 3, 0),
+(1234, 'usuario', 'prueba', 'prueba@prueba.com', 2147483647, '2022-12-29', '81dc9bdb52d04dc20036dbd8313ed055', 3, 0),
+(5678, 'admin', 'prueba', 'prueba@prueba.com', 5678, '2022-12-30', '674f3c2c1a8a6f90461e8a66fb5550ba', 1, 0),
+(5679, 'usu001', 'prueba001', 'prueba@prueba.com', 3000000001, '2000-01-01', '1001', 3, 0),
+(1031150195, 'Yair', 'Millan Ramos', 'yairmillan1@gmail.com', 3017319798, '2023-01-04', '818a3035247139a77c4b600337a2c1e6', 1, 2);
 
 --
 -- Índices para tablas volcadas
@@ -233,13 +259,6 @@ ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `roles_usuario`
---
-ALTER TABLE `roles_usuario`
-  ADD PRIMARY KEY (`rolesid`,`usuarioid`),
-  ADD KEY `fk_rolesUsuario_usuario` (`usuarioid`);
-
---
 -- Indices de la tabla `tiemposuscripcion`
 --
 ALTER TABLE `tiemposuscripcion`
@@ -250,7 +269,8 @@ ALTER TABLE `tiemposuscripcion`
 --
 ALTER TABLE `usuario`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_genero_usuario` (`generoid`);
+  ADD KEY `fk_genero_usuario` (`generoid`),
+  ADD KEY `fk_rol_usuario` (`rol_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -290,25 +310,25 @@ ALTER TABLE `notas`
 -- AUTO_INCREMENT de la tabla `registrosuscripcion`
 --
 ALTER TABLE `registrosuscripcion`
-  MODIFY `reg` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `reg` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `tiemposuscripcion`
 --
 ALTER TABLE `tiemposuscripcion`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=367;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5679;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1031150196;
 
 --
 -- Restricciones para tablas volcadas
@@ -342,17 +362,11 @@ ALTER TABLE `registrosuscripcion`
   ADD CONSTRAINT `fk_usario_registro` FOREIGN KEY (`usuarioid`) REFERENCES `usuario` (`id`);
 
 --
--- Filtros para la tabla `roles_usuario`
---
-ALTER TABLE `roles_usuario`
-  ADD CONSTRAINT `fk_rolesUsuario_usuario` FOREIGN KEY (`usuarioid`) REFERENCES `usuario` (`id`),
-  ADD CONSTRAINT `fk_roles_rolesUsuario` FOREIGN KEY (`rolesid`) REFERENCES `roles` (`id`);
-
---
 -- Filtros para la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  ADD CONSTRAINT `fk_genero_usuario` FOREIGN KEY (`generoid`) REFERENCES `genero` (`id`);
+  ADD CONSTRAINT `fk_genero_usuario` FOREIGN KEY (`generoid`) REFERENCES `genero` (`id`),
+  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
